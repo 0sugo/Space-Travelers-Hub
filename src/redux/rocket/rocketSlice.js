00d8be377ada08ead177fb1af9f3802ac2c60454
0sugo <josugo26@gmail.com>
2023-05-17 08:09:48 +0300

    Fetch rockets and store in redux

---
+++ src/redux/rocket/rocketSlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://api.spacexdata.com/v3/rockets';
+
+export const fetchRockets = createAsyncThunk('rockets/getter', async () => {
+  try {
+    const response = await axios(url);
+    // console.log(response.data);
+    return response.data;
+  } catch (error) {
+    return ('Found this error', error);
+  }
+});
+
+const initialState = {
+  rockets: [],
+  isLoading: false,
+};
+const rocketSlice = createSlice({
+  name: 'all-Rockets',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRockets.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchRockets.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.rockets = action.payload.map((rocket) => ({
+          id: rocket.rocket_id,
+          name: rocket.rocket_name,
+          type: rocket.rocket_type,
+          flickr_images: rocket.flickr_images,
+        }));
+      })
+      .addCase(fetchRockets.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
+
+});
+
+export default rocketSlice.reducer;
