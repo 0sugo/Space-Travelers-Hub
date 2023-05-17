e0263c85ff01fea3763da77cc6df0a5351977f71
0sugo <josugo26@gmail.com>
2023-05-17 08:10:48 +0300

    Add dispatch to fetch rockets on application launch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Navbar from './components/Navbar';
 import Rocket from './components/Rocket';
 import Profile from './components/Profile';
 import Missions from './components/Missions';
+import { fetchRockets } from './redux/rocket/rocketSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
