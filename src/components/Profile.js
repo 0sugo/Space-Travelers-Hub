6d20ba396cfb3cc90d28c7b3492b225451673120
citec-47 <mauricendonyi40@gmail.com>
2023-05-17 12:48:49 -0700

    Adding margintop

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,7 +10,7 @@ function Profile() {
   const joinMission = mission.filter((mission) => mission.reservation === true);
 
   return (
-    <div className={styles.mycontainer}>
+    <div className={styles.container}>
       <MyMissions data={joinMission} />
     </div>
   );
