887d65e8ea1c3eb36b6f68aae4942c36fa8a03ff
citec-47 <mauricendonyi40@gmail.com>
2023-05-17 05:29:43 -0700

    fetch mission API

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,66 @@
-import React from 'react';
+/* eslint-disable import/extensions */
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchMissions,
+  missionStatus,
+  selectsAllMissions,
+} from '../redux/mission/missionSlice';
+import style from './Missio.module.css';
 
-const Missions = () => (
-  <div>Missions</div>
-);
+import Button from '../missionButton/Boton';
 
-export default Missions;
+const Mission = () => {
+  const { mission } = useSelector(selectsAllMissions);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+  const [reservation, setIsreservation] = useState(false);
+
+  const handlemission = (id) => {
+    dispatch(missionStatus(id));
+    setIsreservation(!reservation);
+  };
+  return (
+    <div className={style.container}>
+      <table>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th className={style.stutus}>Status</th>
+            <th className={style.but_join}> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {mission.map((mission, e) => (
+            <tr
+              key={mission.mission_id}
+              className={e % 2 === 0 ? style.white : style.grey}
+            >
+              <td className={style.title}>{mission.mission_name}</td>
+              <td className={style.desc}>{mission.description}</td>
+              <td className={style.stutus_update}>
+                <span
+                  className={mission.reservation ? style.member : style.nmember}
+                >
+                  {mission.reservation ? 'Active Member' : 'Not a member'}
+                </span>
+              </td>
+              <td className={style.stutus_update}>
+                <Button
+                  title={mission.reservation ? 'Leave mission' : 'Join Mission'}
+                  className={mission.reservation ? style.btn : style.joind}
+                  onReserve={() => handlemission(mission.mission_id)}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Mission;
