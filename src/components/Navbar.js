99891cc3fc378ba3880d6f8ca430eb35767990c9
0sugo <josugo26@gmail.com>
2023-05-17 08:16:17 +0300

    Fix route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
   <div className="nav-bar flexer">
-    <NavLink to="Rocket">Rockets</NavLink>
+    <NavLink to="/">Rockets</NavLink>
     <NavLink to="Missions">Missions</NavLink>
     <NavLink to="Profile">My Profile</NavLink>
   </div>
