e9ac157a1ec0e0ea4e3033b295f0bca7c1f0a112
citec-47 <mauricendonyi40@gmail.com>
2023-05-18 16:55:17 -0700

    All test are passing

---
+++ src/tests/Profile.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import Myprofile from '../components/Profile';
+import store from '../redux/store';
+
+describe('Test Profile components', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Myprofile />
+        </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
